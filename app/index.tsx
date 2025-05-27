a6f92b54dde96d864140d53e4e6694e054a3fa65
julia <julia.zhyvylo@isolutions.mk>
2025-05-27 10:42:14 +0200

    initial

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import MemberDetails from "@/components/MemberDetails";
+import { View } from "react-native";
+
+export default function Home() {
+  return (
+    <View>
+      {/* <GenerationsList/> */}
+      {/* <MembersList/> */}
+      <MemberDetails/>
+    </View>
+  );
+}
